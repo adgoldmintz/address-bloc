62058ff409093fda6bc578849cdaa5d9ad3efcfe
AGoldmintz <agoldmintz@gmail.com>
2018-12-13 14:06:34 -0600

    Implemented search and delete contacts

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -29,12 +29,90 @@ module.exports = class ContactController {
                 }
             },
         ];
-    }
-        addContact(name, phone, email) {
-            return Contact.create({name, phone, email})
-        }
 
-        getContacts() {
-            return Contact.findAll()
-        }
+        this.searchQuestions = [
+            {
+                type: "input",
+                name: "name",
+                message: "Name of contact to search - ",
+                validate(val){
+                    return val !== "";
+                }
+            }
+        ];
+
+        this.showContactQuestions = [
+            {
+                type: "list",
+                name: "selected",
+                message: "Please choose from an option below: ",
+                choices: [
+                    "Delete contact",
+                    "Main menu"
+                ]
+            }
+        ];
+
+        this.deleteConfirmQuestions = [
+            {
+                type: "confirm",
+                name: "confirmation",
+                message: "are you sure you want to delete this contact?"
+            }
+        ];
     }
+
+
+    addContact(name, phone, email) {
+        return Contact.create({name, phone, email})
+    }
+
+    getContacts() {
+        return Contact.findAll()
+    }
+
+    iterativeSearch(contacts, target) {
+        for(let contact of contacts) {
+            if(contact.name.toLowerCase() === target.toLowerCase()){
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    binarySearch(contacts, target) {
+        let min = 0;
+        let max = contacts.length - 1;
+        let mid;
+            
+        while(min <= max) {
+        // update 'mid' to be the index in the middle of the portion of the array we are searching then round it down using Math.floor
+            mid = Math.floor((min + max) / 2);
+            let currentContact = contacts[mid];
+      
+        // #2 eliminate upper bound if target comes before element at mid, or eliminate lower bound if target comes after mid, otherwise return  element at mid
+            if(currentContact.name > target) { //target is before element at mid, eliminate upper bound
+                max = mid - 1;
+            } 
+            else if(currentContact.name < target){ //target is after element at mid, eliminate lower bound
+                min = mid - 1;
+            } else 
+            { //element found, return it
+                return contacts[mid];
+            }
+        }
+        return null;
+    }
+
+    search(name) {
+        return Contact.findOne({
+            where: {name}
+        });
+    }
+
+    delete(id) {
+        return Contact.destroy({
+            where: {id}
+        })
+    }
+}
